7aae3ee8444bcdc86544142abefd4c7f0915aed9
Konstantin Vershigora <3024664114+kostip@users.noreply.github.com>
2026-07-25 14:12:44 +0700

    Improve accessibility and consolidate contacts

---
+++ src/data/contacts.ts
@@ -0,0 +1,6 @@
+export const contactDetails = {
+  telegramUrl: 'https://t.me/k0stip',
+  emailAddress: 'versh.konstantin@gmail.com',
+  emailUrl: 'mailto:versh.konstantin@gmail.com',
+  hhResumeUrl: 'https://novosibirsk.hh.ru/resume/2dada270ff06c0c99b0039ed1f474e644b6e41',
+} as const;

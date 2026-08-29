d95eb3e2f60ba94c072e034167032ba26e14f942
Konstantin Vershigora <3024664114+kostip@users.noreply.github.com>
2026-08-29 17:06:03 +0700

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 // @ts-check
 import { satteri } from '@astrojs/markdown-satteri';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import { createProjectImageAttributesPlugin } from './src/utils/projectImages.mjs';
 
+const indexedPaths = new Set([
+  '/',
+  '/projects/gor/',
+  '/projects/tabs4me/',
+  '/projects/bravo-optika/',
+  '/projects/optima-servis/',
+  '/projects/amelisoul/',
+]);
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://versh-konstantin.ru',
+  integrations: [
+    sitemap({
+      filter: (page) => indexedPaths.has(new URL(page).pathname),
+    }),
+  ],
   markdown: {
     processor: satteri({
       hastPlugins: [createProjectImageAttributesPlugin],
